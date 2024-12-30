34b67979ec3758e47815518180800483cc59b570
raytomely <45993451+raytomely@users.noreply.github.com>
2024-12-30 11:35:01 +0100

    Add files via upload

---
+++ learn_opengl/source_code/Model Loading/Model/main.cpp
@@ -0,0 +1,211 @@
+#include <SDL/SDL.h>
+#include "glad.h"
+
+#include "glm/glm.hpp"
+#include "glm/gtc/matrix_transform.hpp"
+#include "glm/gtc/type_ptr.hpp"
+
+#include "shader_m.h"
+#include "camera.h"
+#include "model.h"
+#include "filesystem.h"
+
+#include <iostream>
+
+void processInput(void);
+void sleep(void);
+
+// settings
+const unsigned int SCR_WIDTH = 800;
+const unsigned int SCR_HEIGHT = 600;
+
+// camera
+Camera camera(glm::vec3(0.0f, 0.0f, 3.0f));
+float lastX = SCR_WIDTH / 2.0f;
+float lastY = SCR_HEIGHT / 2.0f;
+bool firstMouse = true;
+
+// timing
+float deltaTime = 0.0f;	// time between current frame and last frame
+float lastFrame = 0.0f;
+
+// lighting
+glm::vec3 lightPos(1.2f, 1.0f, 2.0f);
+
+bool main_loop = true;
+SDL_Event event;
+Uint8* keys;
+
+int main(int argc, char *argv[])
+{
+    SDL_Init(SDL_INIT_VIDEO);
+    SDL_WM_SetCaption("LearnOpenGL",NULL);
+    SDL_SetVideoMode(640, 480, 32, SDL_OPENGL);//|SDL_RESIZABLE);
+
+    // glad: load all OpenGL function pointers
+    // ---------------------------------------
+    if (!gladLoadGLLoader((GLADloadproc)SDL_GL_GetProcAddress))
+    {
+        std::cout << "Failed to initialize GLAD" << std::endl;
+        return -1;
+    }
+
+
+    // configure global opengl state
+    // -----------------------------
+    glEnable(GL_DEPTH_TEST);
+
+    // build and compile shaders
+    // -------------------------
+    Shader ourShader("1.model_loading.vs", "1.model_loading.fs");
+
+    // load models
+    // -----------
+    Model ourModel(FileSystem::getPath("Orca/cbu4body.obj"));
+
+
+    // draw in wireframe
+    //glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+
+    // render loop
+    // -----------
+    while (main_loop)
+    {
+        // per-frame time logic
+        // --------------------
+        float currentFrame = static_cast<float>(SDL_GetTicks());
+        deltaTime = currentFrame - lastFrame;
+        lastFrame = currentFrame;
+
+        // input
+        // -----
+        processInput();
+
+        // render
+        // ------
+        glClearColor(0.05f, 0.05f, 0.05f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+        // don't forget to enable shader before setting uniforms
+        ourShader.use();
+
+        // view/projection transformations
+        glm::mat4 projection = glm::perspective(glm::radians(camera.Zoom), (float)SCR_WIDTH / (float)SCR_HEIGHT, 0.1f, 100.0f);
+        glm::mat4 view = camera.GetViewMatrix();
+        ourShader.setMat4("projection", projection);
+        ourShader.setMat4("view", view);
+
+        // render the loaded model
+        glm::mat4 model = glm::mat4(1.0f);
+        model = glm::translate(model, glm::vec3(0.0f, 0.0f, 0.0f)); // translate it down so it's at the center of the scene
+        model = glm::scale(model, glm::vec3(1.0f, 1.0f, 1.0f));	// it's a bit too big for our scene, so scale it down
+        ourShader.setMat4("model", model);
+        ourModel.Draw(ourShader);
+
+        SDL_GL_SwapBuffers();
+        sleep();
+    }
+
+    SDL_Quit();
+    return 0;
+}
+
+// process all input: query whether relevant keys are pressed/released this frame and react accordingly
+// ---------------------------------------------------------------------------------------------------------
+void processInput(void)
+{
+    if(SDL_PollEvent(&event) == 1)
+    {
+        switch(event.type)
+        {
+            case SDL_QUIT:
+                main_loop = false;
+                break;
+            /*case SDL_VIDEORESIZE:
+                SDL_SetVideoMode(event.resize.w, event.resize.h, 32, SDL_OPENGL|SDL_RESIZABLE);
+                glViewport(0, 0, event.resize.w, event.resize.h);
+                break;*/
+            case SDL_MOUSEMOTION:
+            {
+                float xpos = static_cast<float>(event.motion.x);
+                float ypos = static_cast<float>(event.motion.y);
+
+                if (firstMouse)
+                {
+                    lastX = xpos;
+                    lastY = ypos;
+                    firstMouse = false;
+                }
+
+                float xoffset = xpos - lastX;
+                float yoffset = lastY - ypos; // reversed since y-coordinates go from bottom to top
+
+                lastX = xpos;
+                lastY = ypos;
+
+                camera.ProcessMouseMovement(xoffset, yoffset);
+                break;
+            }
+            case SDL_MOUSEBUTTONDOWN:
+            {
+                if (event.button.button == SDL_BUTTON_WHEELUP)
+                {
+                    camera.ProcessMouseScroll(static_cast<float>(2.0f));
+                }
+                else if (event.button.button == SDL_BUTTON_WHEELDOWN)
+                {
+                    camera.ProcessMouseScroll(static_cast<float>(-2.0f));
+                }
+                break;
+            }
+
+        }
+    }
+
+    keys = SDL_GetKeyState(NULL);
+
+    if(keys[SDLK_ESCAPE])
+        main_loop = 0;
+
+    if(keys[SDLK_w])
+        camera.ProcessKeyboard(FORWARD, deltaTime);
+    else if(keys[SDLK_a])
+        camera.ProcessKeyboard(BACKWARD, deltaTime);
+    if(keys[SDLK_s])
+        camera.ProcessKeyboard(LEFT, deltaTime);
+    else if(keys[SDLK_d])
+        camera.ProcessKeyboard(RIGHT, deltaTime);
+
+    if(keys[SDLK_UP])
+        camera.ProcessMouseMovement(0, 10);
+    else if(keys[SDLK_DOWN])
+        camera.ProcessMouseMovement(0, -10);
+    if(keys[SDLK_LEFT])
+        camera.ProcessMouseMovement(-10, 0);
+    else if(keys[SDLK_RIGHT])
+        camera.ProcessMouseMovement(10, 0);
+
+}
+
+void sleep(void)
+{
+    static int old_time = 0,  actual_time = 0;
+    actual_time = SDL_GetTicks();
+    if (actual_time - old_time < 16) // if less than 16 ms has passed
+    {
+        SDL_Delay(16 - (actual_time - old_time));
+        old_time = SDL_GetTicks();
+    }
+    else
+    {
+        old_time = actual_time;
+    }
+}
+
+
+
+
+
+
+
+
